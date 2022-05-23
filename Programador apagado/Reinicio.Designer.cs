c7944660f7ed96ba10fabe37fb7845386b1a9613
Solhdeck <amilcarcp@trisonworld.com>
2022-05-23 14:33:54 +0200

    Subo el programa en si

---
+++ Programador apagado/Reinicio.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Programador_apagado
+{
+    partial class Reinicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Horas1 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Manual = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Horas1
+            // 
+            this.Horas1.Font = new System.Drawing.Font("Verdana", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Horas1.Location = new System.Drawing.Point(12, 12);
+            this.Horas1.Name = "Horas1";
+            this.Horas1.Size = new System.Drawing.Size(167, 101);
+            this.Horas1.TabIndex = 0;
+            this.Horas1.Text = "1h";
+            this.Horas1.UseVisualStyleBackColor = true;
+            this.Horas1.Click += new System.EventHandler(this.Horas1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(185, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 101);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "30m";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(63, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 78);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ":";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Verdana", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(103, 119);
+            this.textBox2.MaxLength = 2;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(91, 85);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "00";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Verdana", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(22, 118);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(56, 85);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "1";
+            // 
+            // Manual
+            // 
+            this.Manual.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Manual.Location = new System.Drawing.Point(206, 125);
+            this.Manual.Name = "Manual";
+            this.Manual.Size = new System.Drawing.Size(152, 78);
+            this.Manual.TabIndex = 6;
+            this.Manual.Text = "Manual";
+            this.Manual.UseVisualStyleBackColor = true;
+            this.Manual.Click += new System.EventHandler(this.Manual_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Verdana", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(61, 215);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(249, 66);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Reinicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 293);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Manual);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Horas1);
+            this.Name = "Reinicio";
+            this.Text = "Programar reinicio";
+            this.Load += new System.EventHandler(this.Reinicio_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Horas1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Manual;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Programador apagado/Reinicio.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programador_apagado
+{
+    public partial class Reinicio : Form
+    {
+        public Reinicio()
+        {
+            InitializeComponent();
+        }
+
+        private void Reinicio_Load(object sender, EventArgs e)
+        {
+            int posX = (Screen.FromControl(this).Bounds.Width - this.ClientSize.Width)/2;
+            int posY = (Screen.FromControl(this).Bounds.Height - this.ClientSize.Height)/2;
+            this.Location = new System.Drawing.Point(posX, posY);
+        }
+
+        private void Horas1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process ProcesoActivacion1 = Process.Start("shutdown", "-s -t 3600");
+            }
+            catch (Exception) { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process ProcesoActivacion1 = Process.Start("shutdown", "-s -t 1800");
+            }
+            catch (Exception) { }
+        }
+
+        private void Manual_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                int tiempo = int.Parse(textBox1.Text.ToString()) * 3600 + int.Parse(textBox2.Text.ToString()) * 60;
+                Process ProcesoActivacion1 = Process.Start("shutdown", "-s -t " + tiempo.ToString());
+            }
+            catch (Exception) { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process ProcesoActivacion1 = Process.Start("shutdown", "-a");
+        }
+    }
+}
